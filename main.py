d50ac5cf92028db05460194f3dfa325397be98e5
Blake Gifford <80912809+blake-gifford@users.noreply.github.com>
2025-02-28 14:25:36 -0700

    create main

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+import sys
+import os
+
+from scripts.extract import extract_data
+from scripts.transform import transform_data
+from scripts.load import load_data
+
+def run_pipeline(input_path: str, output_file: str): 
+    try:
+        # check if outfile exists
+        outfile = os.path.exists(output_file)
+        if not outfile:
+            os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+        # pull json data
+        df = extract_data(input_path)
+        df = transform_data(df)
+        load_data(output_file, df)
+
+        print("Pipeline ran successfully")
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Pull CLI arguments and remove first item
+args = sys.argv[1:]
+
+# Pass CLI args to the pipeline
+# Allow default file paths for source & destination
+parser = argparse.ArgumentParser(description="ELT Pipeline for Domotz data")
+parser.add_argument('data_path', nargs='?', default='./data/data.json', help='Path to the domotz data')
+parser.add_argument('output_file', nargs='?', default='./data/output.json', help='Path to the output file')
+args = parser.parse_args()
+
+run_pipeline(args.data_path, args.output_file)
