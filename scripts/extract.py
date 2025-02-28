5642dd19215e37416808ac300604361af69faf30
Blake Gifford <80912809+blake-gifford@users.noreply.github.com>
2025-02-28 16:20:15 -0700

    update

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import json
 
-def extract_data(file_path: str):
-    df = pd.read_json(file_path)
-    return df
+def extract_data(file_path: str, input_data: dict):
+    if input_data:
+        df = json.loads(input_data)
+        return df
+    else:
+        df = pd.read_json(file_path)
+        return df
