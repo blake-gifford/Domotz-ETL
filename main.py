58a30138e03d447635edb4f3a8d25394c4b51b90
Blake Gifford <80912809+blake-gifford@users.noreply.github.com>
2025-02-28 16:20:10 -0700

    update

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from scripts.extract import extract_data
 from scripts.transform import transform_data
 from scripts.load import load_data
 
-def run_pipeline(input_path: str, output_file: str): 
+def run_pipeline(input_path: str, output_file: str, input_data: dict): 
     try:
+        # if json data is passed dont parse a file, handle the data
+        # option to return data to stdout
         # check if outfile exists
         outfile = os.path.exists(output_file)
         if not outfile:
-            os.makedirs(os.path.dirname(f"./data/{output_file}"), exist_ok=True)
+            os.makedirs(os.path.dirname(
+                f"./data/{output_file}"), 
+                exist_ok=True
+            )
 
         # pull json data
         df = extract_data(input_path)
+        print(sys.getsizeof(df))
         df = transform_data(df)
         load_data(output_file, df)
 
@@ -28,8 +34,9 @@ args = sys.argv[1:]
 # Pass CLI args to the pipeline
 # Allow default file paths for source & destination
 parser = argparse.ArgumentParser(description="ELT Pipeline for Domotz data")
-parser.add_argument('data_path', nargs='?', default='./data/data.json', help='Path to the domotz data')
-parser.add_argument('output_file', nargs='?', default='./data/output.json', help='Path to the output file')
+parser.add_argument('--data', nargs='?', default=None, help='JSON data passed as a string')
+parser.add_argument('--data_path', nargs='?', default='./data/data.json', help='Path to the domotz data')
+parser.add_argument('--output_file', nargs='?', default='./data/output.json', help='Path to the output file')
 args = parser.parse_args()
 
-run_pipeline(args.data_path, args.output_file)
+run_pipeline(args.data_path, args.output_file, args.data)
